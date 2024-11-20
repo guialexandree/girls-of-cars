3a6117e534fecb34ea4abc6a47518b1e8b9ae45e
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2024-11-20 15:33:45 -0300

    feat: melhorias tela home

--- src/presentation/pages/store/components/header/index.tsx
+++ src/presentation/pages/store/components/header/index.tsx
@@ -1,4 +1,4 @@
-import { Box, useMediaQuery, useTheme } from '@mui/material'
+import { Box, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
 
 interface HeaderProps {
   prop?: boolean
@@ -9,13 +9,30 @@ export const Header: React.FC<HeaderProps> = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
-    <Box component='header'>
+    <Box component="header" width='100vw'>
       <Box
-        component='img'
-        src={isMobile ? '/img/header-m-store.png' : '/img/header-store.png'}
-        alt="Bem Vinda - Girls of Cars" maxWidth='100%'
+        component="img"
+        src={isMobile ? '/img/header-m-store.jpg' : '/img/header-store.jpg'}
+        alt="Bem Vinda - Girls of Cars"
+        maxWidth="100%"
+        sx={{ position: 'absolute', top: 0, left: 0 }}
+      />
+
+      <Stack
+        component="section"
+        alignItems="center"
+        justifyContent="center"
+        position='relative'
+        flex={1}
+        height="50vh"
       >
-      </Box>
+        <Typography color="white" variant="h3">
+          BEM VINDA
+        </Typography>
+        <Typography color="grey" variant="h6">
+          LOJINHA
+        </Typography>
+      </Stack>
     </Box>
   )
 }
